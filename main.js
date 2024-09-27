42625dd6f039b3d6205ce850230db3a08bea4230
at00687@rvt.lv <at00687@rvt.lv>
2024-09-27 15:49:31 +0300

    Created js file,dark mode on hold

---
+++ main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    
+    document.body.classList.toggle("dark-mode-body");
+    var polaroid = document.getElementsByClassName("polaroid");
+    polaroid.classList.toggle("dark-mode-body");
+
+}
