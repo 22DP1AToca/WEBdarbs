eb4cb06de35453d0b579f6ec67eab2091c899dcb
at00687@rvt.lv <at00687@rvt.lv>
2024-10-02 13:08:23 +0300

    completed module and gradient

--- main.js
+++ main.js
@@ -5,28 +5,46 @@
 
     // Get the modal
     var modal = document.getElementById("modal1");
+    var modal2 = document.getElementById("modal2");
+    var modal3 = document.getElementById("modal3");
 
     // Get the button that opens the modal
     var btn = document.getElementById("learn_more1");
+    var btn2 = document.getElementById("learn_more2");
+    var btn3 = document.getElementById("learn_more3");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on the button, open the modal
     btn.onclick = function(){
-        console.log(1);
         modal.style.display = "block";
-    
     }
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
+    btn2.onclick = function(){
+        modal2.style.display = "block";
     }
+
+    btn3.onclick = function(){
+        modal3.style.display = "block";
+    }
+
+    function close_model(){
+        modal.style.display = "none";
+        modal2.style.display = "none";
+        modal3.style.display = "none";
+    }
+
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
+        }
+
+        if (event.target == modal2) {
+            modal2.style.display = "none";
+        }
+
+        if (event.target == modal3) {
+            modal3.style.display = "none";
         }
     }
