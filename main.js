3fca9e1fd03453af0792fd803e49793faae39a93
22DP1AToca <128512901+22DP1AToca@users.noreply.github.com>
2024-10-07 00:22:16 +0300

    Added dark mode, dropdown

--- main.js
+++ main.js
@@ -1,50 +1,66 @@
+//-----Dark mode-------
+var toggleButton = document.getElementsByClassName("dark-mode-toggle");
 
-    // document.body.classList.toggle("dark-mode-body");
-    // var polaroid = document.getElementsByClassName("polaroid");
-    // polaroid.classList.toggle("dark-mode-body");
+toggleButton[0].onclick = function(){
+  document.body.classList.toggle('dark-mode');
+};
 
-    // Get the modal
-    var modal = document.getElementById("modal1");
-    var modal2 = document.getElementById("modal2");
-    var modal3 = document.getElementById("modal3");
+toggleButton[1].onclick = function(){
+    document.body.classList.toggle('dark-mode');
+};
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("learn_more1");
-    var btn2 = document.getElementById("learn_more2");
-    var btn3 = document.getElementById("learn_more3");
+//----Mobile nav--------
+var mobileNav = document.querySelector(".mobile-nav");
+var navToggleLabel = document.querySelector('.hamburger');
+var navLinks = mobileNav.querySelector('ul');
+
+navToggleLabel.onclick = function(){
+    navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+}
 
 
-    // When the user clicks on the button, open the modal
-    btn.onclick = function(){
-        modal.style.display = "block";
-    }
+// ----Model------------
+var modal = document.getElementById("modal1");
+var modal2 = document.getElementById("modal2");
+var modal3 = document.getElementById("modal3");
 
-    btn2.onclick = function(){
-        modal2.style.display = "block";
-    }
+ // Get the button that opens the modal
+var btn = document.getElementById("learn_more1");
+var btn2 = document.getElementById("learn_more2");
+var btn3 = document.getElementById("learn_more3");
 
-    btn3.onclick = function(){
-        modal3.style.display = "block";
-    }
 
-    function close_model(){
+// When the user clicks on the button, open the modal
+btn.onclick = function(){
+    modal.style.display = "block";
+}
+
+btn2.onclick = function(){
+    modal2.style.display = "block";
+}
+
+btn3.onclick = function(){
+    modal3.style.display = "block";
+}
+
+function close_model(){
+    modal.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
         modal.style.display = "none";
+    }
+
+    if (event.target == modal2) {
         modal2.style.display = "none";
+    }
+
+    if (event.target == modal3) {
         modal3.style.display = "none";
     }
-
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-
-        if (event.target == modal2) {
-            modal2.style.display = "none";
-        }
-
-        if (event.target == modal3) {
-            modal3.style.display = "none";
-        }
-    }
+}
